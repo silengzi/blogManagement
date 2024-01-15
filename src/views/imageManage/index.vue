f818ec5e31f9028a0440e96c6b635b4f6dff3923
silengzi <474960201@qq.com>
2024-01-15 18:08:20 +0800

    常规修改

--- src/views/imageManage/index.vue
+++ src/views/imageManage/index.vue
@@ -1,72 +1,104 @@
 <template>
   <div>
-    <blogManageTopNav></blogManageTopNav>
-    <div style="margin-bottom: 20px">
-      <el-button type="primary">添 加</el-button>
-      <el-button type="danger">批量删除</el-button>
+    <div class="main" v-show="scene==0">
+      <blogManageTopNav></blogManageTopNav>
+      <div style="margin-bottom: 20px">
+        <el-button type="primary">添 加</el-button>
+        <el-button type="danger">批量删除</el-button>
+      </div>
+      <el-table border :data="imageList">
+        <el-table-column
+          type="selection"
+          align="center"
+          width="70"
+        ></el-table-column>
+        <el-table-column
+          type="index"
+          label="序号"
+          width="90"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          label="图片名称"
+          prop="name"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          label="默认图片"
+          align="center"
+          width="110"
+        >
+          <template slot-scope="{row, $index}">
+              <img :src="row.defaultImg" alt="" style="width: 70px; height: 70px; object-fit: cover;">
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          align="center"
+        >
+          <template slot-scope="{row}">
+              <el-button
+                  type="warning"
+                  icon="el-icon-edit"
+                  size="mini"
+                  title="编辑图片"
+                  @click="imageEdit(row)"
+              ></el-button>
+              <el-button
+                  type="primary"
+                  icon="el-icon-info"
+                  size="mini"
+                  title="图片详情"
+                  @click="getImageInfo(row)"
+              ></el-button>
+              <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  title="删除"
+              ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+          :current-page="page"
+          :total="total"
+          :page-size="limit"
+          :page-sizes="[3, 10, 20]"
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          style="padding: 20px 0"
+      ></el-pagination>
     </div>
-    <el-table border :data="imageList">
-      <el-table-column
-        type="selection"
-        align="center"
-      ></el-table-column>
-      <el-table-column
-        type="index"
-        label="序号"
-        width="90"
-        align="center"
-      ></el-table-column>
-      <el-table-column
-        label="图片名称"
-        prop="name"
-        align="center"
-      ></el-table-column>
-      <el-table-column
-        label="默认图片"
-        align="center"
-      >
-        <template slot-scope="{row, $index}">
-            <img :src="row.defaultImg" alt="" style="width: 70px; height: 70px; object-fit: cover;">
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="操作"
-        align="center"
-      >
-        <template slot-scope="{row}">
-            <el-button
-                type="primary"
-                icon="el-icon-info"
-                size="mini"
-                title="图片详情"
-            ></el-button>
-            <el-button
-                type="danger"
-                icon="el-icon-delete"
-                size="mini"
-                title="删除"
-            ></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-        :current-page="page"
-        :total="total"
-        :page-size="limit"
-        :page-sizes="[3, 10, 20]"
-        layout="prev, pager, next, jumper, ->, sizes, total"
-        style="padding: 20px 0"
-    ></el-pagination>
-    
+    <imageEdit v-show="scene==1"></imageEdit>
+    <el-drawer :visible.sync="show" size="50%" v-for="(i, index) in imageList" :key="i.id">
+      <el-row>
+        <el-col :span="5">名称</el-col>
+        <el-col  :span="16">{{i.name}}</el-col>
+      </el-row>
+      <el-row >
+        <el-col :span="5">描述</el-col>
+        <el-col  :span="16">aa</el-col>
+      </el-row>
+      <el-row >
+        <el-col :span="5">图片</el-col>
+        <el-col  :span="16">
+          <!-- <el-carousel height="210px"> -->
+            <img :src="i.defaultImg" alt="" style="width: 100%; height: 200px; object-fit: cover;">
+          <!-- </el-carousel> -->
+        </el-col>
+      </el-row>
+    </el-drawer>
   </div>
 </template>
 
 <script>
 import blogManageTopNav from '@/components/blogManageTopNav/index.vue'
+import imageEdit from './imageEdit'
 export default {
   name: '',
   components: {
-    blogManageTopNav
+    blogManageTopNav,
+    imageEdit,
   },
   data() {
     return {
@@ -74,7 +106,9 @@ export default {
         page: 1,
         total: 0,
         limit: 3,
-        dialogUserVisible: false
+        dialogUserVisible: false,
+        scene: 0,
+        show: false,
     };
   },
   mounted() {
@@ -90,10 +124,22 @@ export default {
     ]
   },
   methods: {
-    
+    imageEdit(row) {
+      this.scene = 1
+    },
+    getImageInfo(row) {
+      this.show = true
+    }
   }
 };
 </script>
 
-<style scoped lang="">
+<style scoped lang="less">
+  .el-row .el-col-5 {
+    font-size: 18px;
+    text-align: right;
+  }
+  .el-col {
+    margin: 10px;
+  }
 </style>
