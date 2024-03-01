f7c9e14d25594d5557e8ed8a022fda3e910939e8
silengzi <474960201@qq.com>
2024-03-01 10:35:52 +0800

    修改服务器地址

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://123.57.246.82:8888',
+        target: 'http://101.42.46.104:8888',
         changeOrigin: true,   // 开启代理，在本地创建一个虚拟服务 
         pathRewrite: {
           '^/api': ''
